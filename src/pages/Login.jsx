555b4ec28f30bb7867e00a78ed380412cb4c7c1a
No User <jason.vg24@gmail.com>
2026-06-02 01:37:46 +0000

    Visual edits

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -109,7 +109,7 @@ export default function Login() {
             
           </div>
         </div>
-        <Button type="submit" className="w-full h-12 font-medium bg-teal-400" disabled={loading}>
+        <Button type="submit" className="w-full h-12 font-medium bg-[hsl(var(--chart-1))]" disabled={loading}>
           {loading ?
           <>
               <Loader2 className="w-4 h-4 mr-2 animate-spin" />
