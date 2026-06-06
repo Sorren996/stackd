527ccc3534a8a8411a54974aae428557751b6ccd
No User <jason.vg24@gmail.com>
2026-06-06 17:39:38 +0000

    Manual code change

--- src/components/ActiveInsulinBanner.jsx
+++ src/components/ActiveInsulinBanner.jsx
@@ -241,7 +241,7 @@ const heroOrbColor =
 
       <div className="pt-2 pb-6 -mx-4 px-4">
         {/* Ambient breathing background orb */}
-        <div className="absolute left-1/2 -translate-x-1/2 top-16 w-72 h-72 pointer-events-none -z-10 overflow-y-hidden">
+        <div className="absolute left-1/2 -translate-x-1/2 top-16 w-72 h-72 pointer-events-none -z-10 overflow-hidden">
           <motion.div
             animate={{ scale: [1, 1.08, 1], opacity: [0.25, 0.4, 0.25] }}
             transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
