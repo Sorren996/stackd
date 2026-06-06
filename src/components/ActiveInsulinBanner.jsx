a4daaad68ba0a8d4417350e0c58be5cbafa5127d
No User <jason.vg24@gmail.com>
2026-06-06 17:29:15 +0000

    Manual code change

--- src/components/ActiveInsulinBanner.jsx
+++ src/components/ActiveInsulinBanner.jsx
@@ -254,7 +254,7 @@ const heroOrbColor =
         </div>
 
         {/* Primary Glucose Hero */}
-<div className="relative flex flex-col items-center text-center mb-6 pt-6 pb-5 overflow-hidden rounded-[28px]">
+<div className="relative flex flex-col items-center text-center mb-6 pt-6 pb-5 overflow-visible rounded-[28px]">
   {/* Main glucose orb */}
   <motion.div
     aria-visible
