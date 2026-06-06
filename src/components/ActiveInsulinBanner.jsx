e8e41bf65f7489dbcdedf2a9fdb27a088c375c5c
No User <jason.vg24@gmail.com>
2026-06-06 17:19:08 +0000

    Manual code change

--- src/components/ActiveInsulinBanner.jsx
+++ src/components/ActiveInsulinBanner.jsx
@@ -264,7 +264,7 @@ const heroOrbColor =
       opacity: [0.35, 0.85, 0.35],
     }}
     transition={{
-      duration: 3.1,
+      duration: 8.1,
       repeat: Infinity,
       ease: "easeInOut",
     }}
@@ -290,7 +290,7 @@ const heroOrbColor =
       opacity: [0.4, 0, 0.4],
     }}
     transition={{
-      duration: 4,
+      duration: 10,
       repeat: Infinity,
       ease: "easeOut",
     }}
