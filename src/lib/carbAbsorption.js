6143f38d8b908dd7c73c3df33069af85daf4d7fb
No User <jason.vg24@gmail.com>
2026-06-24 18:03:24 +0000

    Manual code change

--- src/lib/carbAbsorption.js
+++ src/lib/carbAbsorption.js
@@ -13,7 +13,7 @@ export const ABSORPTION_PROFILES = {
     peakMin: 65,
     durationMin: 150,
     riseExponent: 1.6,
-    taperExponent: 0,
+    taperExponent: 0.6,
   },
   slow: {
     onsetMin: 25,
