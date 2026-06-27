b80d44a1102cd5f775e5e0a42a6f1c489d6948b1
No User <jason.vg24@gmail.com>
2026-06-27 02:08:45 +0000

    Manual code change

--- src/components/ActiveAlerts.jsx
+++ src/components/ActiveAlerts.jsx
@@ -30,8 +30,7 @@ const phaseConfig = {
 };
 
 export default function ActiveAlerts({ doses = [] }) {
-    const color = profile?.color || "#888888";
-
+  
   const activeDoses = doses
     .map((dose) => ({ dose, status: getDoseStatus(dose) }))
     .filter(({ status }) => status.phase !== "expired")
