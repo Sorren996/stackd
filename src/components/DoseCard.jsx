2add014ba5dcabca23b3186203ce79c67a7354fd
No User <jason.vg24@gmail.com>
2026-06-27 02:04:10 +0000

    Manual code change

--- src/components/DoseCard.jsx
+++ src/components/DoseCard.jsx
@@ -52,7 +52,7 @@ export default function DoseCard({ dose, onDelete, onEdit }) {
   <div
     className="w-7 h-7 rounded-full flex items-center justify-center"
     style={{
-      backgroundColor: `${color}`,
+      backgroundColor: `${color}20`,
       borderColor: `${color}`,
       borderWidth: 1,
       borderStyle: "solid",
