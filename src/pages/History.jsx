f9bba03d2c962cddbbdc94e4ba276dac23e0c3fa
No User <jason.vg24@gmail.com>
2026-06-27 00:47:17 +0000

    Manual code change

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -45,7 +45,7 @@ function CollapsibleDateGroup({ label, count, isOpen, onToggle, children }) {
         transition={{ type: "spring", stiffness: 220, damping: 26 }}
         className="overflow-hidden"
       >
-        <div className="border-t border-white/5 divide-y divide-white/5 p-4">
+        <div className="border-t border-white/5 divide-white/5 space-y-3">
           {children}
         </div>
       </motion.div>
