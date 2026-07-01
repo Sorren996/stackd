f63d5c4094e921d0daa121dc61d2a158d86b2f1f
No User <jason.vg24@gmail.com>
2026-07-01 19:52:13 +0000

    Manual code change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -135,7 +135,7 @@ export default function Layout() {
     let frame = 0;
     const updateSceneOffset = () => {
       frame = 0;
-      setSceneParallaxY(window.scrollY * 0.2);
+      setSceneParallaxY(window.scrollY * 0.7);
     };
     const handleScroll = () => {
       if (frame) return;
